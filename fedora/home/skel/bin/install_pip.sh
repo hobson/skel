95b87d86dfebf65dcb34579357014fc3835a5ac1
Hobson Lane <hobs@localhost.localdomain>
2013-10-20 08:47:32 -0700

    new fedora laptop, steps to install pip, virtualenv, virtualenvwrapper

---
+++ fedora/home/skel/bin/install_pip.sh
@@ -0,0 +1,70 @@
+ifconfig
+ping 192.168.0.3
+nmap -vvv -sn -sL 192.168.0.0/16
+yum install nmap
+sudo yum install nmap
+sudo yum update
+nmap -vvv -sn -sL 192.168.0.0/16
+nmap -vvv -sn -PR 192.168.1.0/24
+nmap -vvv -sn -PR 192.168.0.1/24
+nmap -vvv -sn -PR 192.168.0.0/24
+ifconfig
+ping 192.168.0.1
+ssh bit@bit.storydecoder.com -p 10738
+ping bit.storydecoder.com
+ping 71.59.254.246
+ssh bit@71.59.254.246 -p 10738
+exit
+sudo yum update
+sudo yum complete-transactions
+sudo yum-complete-transactions
+sudo yum-complete-transaction
+python
+sudo yum groupinstall "Development tools"
+man curl
+curl -O https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py
+ls
+mkdir src
+cd src
+mv ../ez_setup.py .
+git clone git+git://github.com/hobsonlane/skel
+git clone git://github.com/hobsonlane/skel
+ls
+cd skel
+ls
+mkdir fedora
+cd fedora
+ls
+mkdir home
+cd home
+mkdir skel
+cd skel
+ls
+mkdir bin
+mkdir src
+cd bin
+cp ~/src/ez_setup.py .
+chmod +x ez_setup.py 
+./ez_setup.py
+nano ez_setup.py 
+ls -al
+yum install nano
+sudo yum install nano
+head ez_setup.py 
+python ez_setup.py
+sudo python ez_setup.py
+curl -O https://raw.github.com/pypa/pip/master/contrib/get-pip.py
+python get-pip.py
+sudo python get-pip.py
+history
+history | cut -c -n 3
+history | cut -c -n3
+history | cut -c 3
+history | cut -c -3
+man cut
+history | cut -c 3-
+history | cut -c 5-
+history | cut -c 8-
+history | cut -c 7-
+
+history | cut -c 8- > install_pip.sh
